dcdba1dd48418f0994d0225c6c967e06ee2e228c
Mateo2208 <mateoandres2208@gmail.com>
2025-03-18 15:12:28 -0700

    Arreglar

--- EJERCICIO_6.cpp
+++ EJERCICIO_6.cpp
@@ -19,15 +19,23 @@ using namespace std;
 class Productos{
   private:
     int id;
+    string nombre;
     float precio;
     int stock;
     public:
-      Productos(int _id = 000, float _precio = 0.00, int _stock = 0){
+      Productos(int _id = 000, float _precio = 0.00, int _stock = 0, string _nombre = "Desconocido"){
+        SetNombre(_nombre);
         SetId(_id);
         SetPrecio(_precio);
         SetStock(_stock);
       }
+      void SetNombre(string _nombre){
+        nombre = _nombre;
+      }
 
+      string GetNombre(){
+        return nombre;
+      }
       void SetId(int _id){
         if(id < 0){
           id = 0;
@@ -65,6 +73,25 @@ class Productos{
       }
 };
 
+class Inventario {
+  private:
+  Vector<Productos> producto;
+
+  public:
+  void Invetario(Productos _producto) {
+    producto.pushBack(_producto);
+  }
+
+  void EliminarProducto(int id) {
+    for (int i = 0; i < producto.getSize(); i++) {
+      if (producto.at(i).GetId() == id) {
+        cout << "Se encontro el producto" << endl;
+      }
+    }
+  }
+};
+
 int main(){
+  Inventario invetario(132, 25, 2, "Mantequilla");
 
 }
