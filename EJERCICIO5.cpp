24dbd5505abd47ea92528ec82fcdb6eff6d0a3e5
Mateo2208 <mateoandres2208@gmail.com>
2025-03-18 11:31:16 -0400

    Ejercicio 5

--- EJERCICIO5.cpp
+++ EJERCICIO5.cpp
@@ -13,19 +13,111 @@ using namespace std;
 
 template <typename T> class VectorCalculator : public Vector<T> {
   public:
-    void Suma(Vector<T>* Vector1, Vector<T>* Vector2) {
+    void Suma(VectorCalculator<T>* Vector1, VectorCalculator<T>* Vector2) {
+      VectorCalculator<T> VectorResuelto;
+      if (Vector1->getSize() >= Vector2->getSize()) {
+        for (int i = 0; i < Vector1->getSize(); i++) {
+          VectorResuelto.pushBack(Vector1->at(i));
+        }
+        for (int i = 0; i < Vector2->getSize(); i++) {
+          VectorResuelto.set(i, (Vector1->at(i) + Vector2->at(i)));
+        }
+      } else {
+
+        for (int i = 0; i < Vector2->getSize(); i++) {
+          VectorResuelto.pushBack(Vector1->at(i));
+        }
+        for (int i = 0; i < Vector1->getSize(); i++) {
+          VectorResuelto.set(i, (Vector1->at(i) + Vector2->at(i)));
+        }
+      }
+      cout << "Array de Suma N1" << endl;
       Vector1->print();
+      cout << "Array de Suma N2" << endl;
       Vector2->print();
+      cout << "Array Resultante" << endl;
+      VectorResuelto.print();
     }
+
+    void Resta(VectorCalculator<T>* Vector1, VectorCalculator<T>* Vector2) {
+        VectorCalculator<T> VectorResuelto;
+        if (Vector1->getSize() >= Vector2->getSize()) {
+          for (int i = 0; i < Vector1->getSize(); i++) {
+            VectorResuelto.pushBack(Vector1->at(i));
+          }
+          for (int i = 0; i < Vector2->getSize(); i++) {
+            VectorResuelto.set(i, (Vector1->at(i) - Vector2->at(i)));
+          }
+        } else {
+
+          for (int i = 0; i < Vector2->getSize(); i++) {
+            VectorResuelto.pushBack(Vector1->at(i));
+          }
+          for (int i = 0; i < Vector1->getSize(); i++) {
+            VectorResuelto.set(i, (Vector1->at(i) - Vector2->at(i)));
+          }
+        }
+        cout << "Array de Resta N1" << endl;
+        Vector1->print();
+        cout << "Array de Resta N2" << endl;
+        Vector2->print();
+        cout << "Array Resultante" << endl;
+        VectorResuelto.print();
+      }
+
+    void Multiplicacion(VectorCalculator<T>* Vector1, VectorCalculator<T>* Vector2) {
+        VectorCalculator<T> VectorResuelto;
+        float Suma = 0.0;
+        if (Vector1->getSize() >= Vector2->getSize()) {
+          for (int i = 0; i < Vector1->getSize(); i++) {
+            VectorResuelto.pushBack(Vector1->at(i));
+          }
+          for (int i = 0; i < Vector2->getSize(); i++) {
+            VectorResuelto.set(i, (Vector1->at(i) * Vector2->at(i)));
+            Suma += VectorResuelto.at(i);
+          }
+
+        } else {
+
+          for (int i = 0; i < Vector2->getSize(); i++) {
+            VectorResuelto.pushBack(Vector1->at(i));
+          }
+          for (int i = 0; i < Vector1->getSize(); i++) {
+            VectorResuelto.set(i, (Vector1->at(i) - Vector2->at(i)));
+            Suma += VectorResuelto.at(i);
+          }
+        }
+        cout << "Array de Multiplicacion N1" << endl;
+        Vector1->print();
+        cout << "Array de Multiplicacion N2" << endl;
+        Vector2->print();
+        cout << "Suma Resultante" << endl;
+        cout << Suma << endl;
+      }
 };
 
 int main(){
   VectorCalculator<int> v1;
   VectorCalculator<int> v2;
+  VectorCalculator<int> v3;
 
-  for (int i = 0; i < 10; i++) {
+  // for (int i = 0; i < 10; i++) {
+  //   v1.pushBack(i);
+  //   v2.pushBack(i);
+  // }
+
+  for (int i = 0; i <= 10; i ++) {
     v1.pushBack(i);
-    v2.pushBack(i);
   }
-  v1.Suma(&v1,&v2);
+
+  for (int i = 0; i <= 10; i ++) {
+    v2.pushBack(i+1);
+  }
+
+  cout << "==============================================" << endl;
+  v1.Suma(&v2, &v1);
+  cout << "==============================================" << endl;
+  v1.Resta(&v2, &v1);
+  cout << "==============================================" << endl;
+  v1.Multiplicacion(&v2, &v1);
 }
