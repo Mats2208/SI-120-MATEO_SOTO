62bc1ab627abe98c7c4435e50c2c575b6e9ad492
Mateo2208 <mateoandres2208@gmail.com>
2025-03-18 08:04:39 -0400

    Actividad 4

--- EJERCICIO3_4.cpp
+++ EJERCICIO3_4.cpp
@@ -18,7 +18,9 @@ template <typename T> class MathVector : public Vector<T> {
 
     void CalcularMediana(MathVector<T>* numeros) {
         MathVector<T> DatosNoAgrupados;
-        DatosNoAgrupados = *numeros;
+        for (int i = 0; i < numeros->getSize(); i++) {
+            DatosNoAgrupados.pushBack(numeros->at(i));
+        }
         Sort(&DatosNoAgrupados);
         bool flag = false;
         int MiddleIndex = 0;
@@ -38,12 +40,13 @@ template <typename T> class MathVector : public Vector<T> {
                 float Mediana = (DatosNoAgrupados.at(MiddleIndex+1) + DatosNoAgrupados.at(MiddleIndex)) / 2.0;
                 cout << "Mediana: " << Mediana<< endl;
             }
+            break;
             case false: {
                 MiddleIndex = (DatosNoAgrupados.getSize() / 2);
                 cout << "Middle Index: " << MiddleIndex << endl;
                 cout << "Mediana: " << DatosNoAgrupados.at(MiddleIndex)<< endl;
             }
-            Default: cout << "No se encontro el Array" << endl;
+            break;
         }
     }
 
@@ -62,6 +65,78 @@ template <typename T> class MathVector : public Vector<T> {
         cout <<"Contenido del array Con Sort" << endl;
         numeros->print();
     }
+
+    void Moda(Vector<T>* numeros) {
+        if (numeros->getSize() == 0) {
+            cout << "El vector está vacío." << endl;
+            return;
+        }
+        Vector<int> frecuencias;
+        for (int i = 0; i < numeros->getSize(); i++) {
+            int contador = 0;
+            for (int j = 0; j < numeros->getSize(); j++) {
+                if (numeros->at(i) == numeros->at(j)) {
+                    contador++;
+                }
+            }
+            frecuencias.pushBack(contador);
+        }
+
+        int maxFrecuencia = frecuencias.at(0);
+        for (int i = 1; i < frecuencias.getSize(); i++) {
+            if (frecuencias.at(i) > maxFrecuencia) {
+                maxFrecuencia = frecuencias.at(i);
+            }
+        }
+
+        cout << "Moda: ";
+        for (int i = 0; i < numeros->getSize(); i++) {
+            if (frecuencias.at(i) == maxFrecuencia) {
+                bool yaImpreso = false;
+                for (int k = 0; k < i; k++) {
+                    if (numeros->at(i) == numeros->at(k)) {
+                        yaImpreso = true;
+                        break;
+                    }
+                }
+                if (!yaImpreso) {
+                    cout << numeros->at(i) << " ";
+                }
+            }
+        }
+        cout << "Frecuencia maxima: " << maxFrecuencia << endl;
+    }
+
+    void Max(Vector<T>* numeros) {
+        cout << "Contenido del array Max" << endl;
+        numeros->print();
+        T max = numeros->at(0);
+        for (int i = 1; i < numeros->getSize(); i++) {
+            if (max > numeros->at(i)) {
+                max = max;
+            } else {
+                max = numeros->at(i);
+            }
+        }
+
+        cout << "El numero maximo es: " << max << endl;
+    }
+
+    void Min(Vector<T>* numeros) {
+        cout << "Contenido del array Min" << endl;
+        numeros->print();
+        T min = numeros->at(0);
+        for (int i = 1; i < numeros->getSize(); i++) {
+            if (min < numeros->at(i)) {
+                min = min;
+            } else {
+                min = numeros->at(i);
+            }
+        }
+
+        cout << "El numero minimo es: " << min << endl;
+    }
+
 };
 
 
@@ -74,14 +149,21 @@ int main(){
     v1.pushBack(2);
     v1.pushBack(1);
     v1.pushBack(3);
+    v1.pushBack(5);
+    v1.pushBack(5);
     v1.pushBack(4);
+    v1.pushBack(1);
     v1.pushBack(8);
+    v1.pushBack(1);
     v1.pushBack(1000);
     cout << "===================" << endl;
     v1.CalcularPromedio(&v1);
     cout << "===================" << endl;
     v1.CalcularMediana(&v1);
     cout << "===================" << endl;
-
+    v1.Moda(&v1);
+    cout << "===================" << endl;
+    v1.Max(&v1);
+    v1.Min(&v1);
     return 0;
 }
